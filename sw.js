65b60169865c9e7fa13a829dc20abc9bec14608e
enriquin12 <151086854+enriquin12@users.noreply.github.com>
2025-12-03 16:02:55 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'minha-carteira-v4';
+const URLS_TO_CACHE = [
+  '/decorg1/',          // página inicial (ajuste conforme seu repositório)
+  '/decorg1/index.html',
+  '/decorg1/pagina2.html',
+  '/decorg1/pagina3.html',
+  '/decorg1/manifest.json',
+  '/decorg1/icons/icon-192.png',
+  '/decorg1/icons/icon-512.png',
+  '/decorg1/pagina1.jpeg',
+  '/decorg1/pagina2.jpeg',
+  '/decorg1/pagina3.jpeg'
+];
+
+// Instala e guarda arquivos no cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+// Intercepta requisições e responde com cache ou rede
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Atualiza cache quando versão muda
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
